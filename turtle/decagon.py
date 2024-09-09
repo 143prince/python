f0597b26dd15146447984efa5211714f445796f6
Prince Singh <princesingh09a7@gmail.com>
2024-09-09 09:41:45 +0530

    turtlecode

---
+++ turtle/decagon.py
@@ -0,0 +1,8 @@
+from turtle import *
+s=getscreen()
+t= Turtle()
+speed('fast')
+for i in range(10):
+    t.fd(80)
+    t.lt(36)
+mainloop()
---
+++ turtle/pentagon.py
@@ -0,0 +1,12 @@
+from turtle import *
+speed("fast")
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+mainloop()
