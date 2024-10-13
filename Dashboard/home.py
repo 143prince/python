9a62157b6f18a089e305a35986b96addefc196b2
Prince Singh <princesingh09a7@gmail.com>
2024-10-13 13:46:51 +0530

    dashboard

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -4,4 +4,10 @@ import plotly.express as px
 import pandas as pd 
 import plotly.graph_objects as go
 
-st.title('Titanic Data Analysis')
+st.markdown('<h1 style="color: red; text-align: center; background-color:white; border: 2px solid red">Titanic Data Analysis </h1>',
+            unsafe_allow_html=True)
+
+st.markdown('<p style="color: Green; text-align: center; margin-top: 20px"> the analysis of titanic data </p>',
+            unsafe_allow_html=True)
+
+st.image('Assets\img.webp',use_column_width=True)
--- Dashboard/pages/Dashboard.py
+++ Dashboard/pages/Dashboard.py
@@ -10,7 +10,6 @@ df = sns.load_dataset('titanic')
 #Set the title of the page 
 st.title('Titanic Data Analysis')
 
-
 st.sidebar.header("Filter Option")
 
 #gender filter 
@@ -37,6 +36,24 @@ filtered_data = df[
     (df['age'] <=max_age)
 ]
 st.dataframe(filtered_data)
+
+# Create a pie chart for gender distribution 
+st.subheader('Gender Distribution')
+gender_count = filtered_data['sex'].value_counts()
+fig = px.pie(names=gender_count.index, values=gender_count.values,
+             title='Gender Distribution')
+st.plotly_chart(fig)
+
+# Create a histogram for age distribution 
+st.subheader('Age Distribution')
+fig = px.histogram(filtered_data, x='age', nbins=20, title='Age Distribution',
+                   labels={'age':'Age', 'count':'Count'})
+st.plotly_chart(fig)
+
+#Violin plot of age distribution by survival status
+#Scatter plot of age vs fare 
+#Box plot of age distribution by passenger class
+#bar chart of servival counts 
 
 
 
